9a8b3b60068f6cfe98fdb83597a25e7e844712dd
Derek Weitzel <djw8605@gmail.com>
2021-10-16 13:18:34 -0500

    Adding verification test

---
+++ verify_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"math/rand"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"bytes"
+	"encoding/binary"
+	"time"
+)
+
+// TestGoodVerify tests the good validation
+func TestGoodVerify(t *testing.T) {
+	goodHeader := Header{}
+	goodHeader.Plen = 16
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, &goodHeader)
+	assert.NoError(t, err, "Failed to write to binary buffer")
+
+	// Generate 8 random bytes
+	token := make([]byte, 8)
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(token)
+	err = binary.Write(buf, binary.BigEndian, token)
+	assert.NoError(t, err, "Failed to write random to binary buffer")
+
+	assert.True(t, verifyPacket(buf.Bytes()), "Failed to verify packet")
+
+
+}
+
+// TestBadVerify tests the validation if the packets are not good (random bits)
+func TestBadVerify(t *testing.T) {
+	badHeader := Header{}
+	badHeader.Plen = 17
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, &badHeader)
+	assert.NoError(t, err, "Failed to write to binary buffer")
+
+	// Generate 8 random bytes
+	token := make([]byte, 8)
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(token)
+	err = binary.Write(buf, binary.BigEndian, token)
+	assert.NoError(t, err, "Failed to write random to binary buffer")
+
+	assert.False(t, verifyPacket(buf.Bytes()), "Failed to verify packet")
+}
