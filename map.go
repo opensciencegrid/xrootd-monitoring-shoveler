446e015eea36aa2ed727add5e034ab6241a7c5bf
Brian Bockelman <bbockelman@morgridge.org>
2023-01-22 17:21:56 -0600

    Allow shoveler to map based on XRDHOST env var

--- map.go
+++ map.go
@@ -13,6 +13,12 @@ var (
 
 // configureMap sets the mapping configuration
 func configureMap() {
+	// When run inside Kubernetes, there may be multiple layers of IP mapping.
+	// Hence, we allow overriding these values based on XRDHOST; XRDHOST is
+	// also the current env var name used to override the advertise hostname =by
+	// the cmsd in the OSG-shipped patches.
+	viper.BindEnv("map.all", "XRDHOST");
+
 	// First, check for the map environment variable
 	mapAll = viper.GetString("map.all")
 
